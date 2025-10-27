dd13d8750c8dc91ec01388ade529b3e8fe7e95ca
Sendhilishaan <ishaansendhil@gmail.com>
2025-10-27 17:21:29 -0400

    feat: added csv reading and displaying data points

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+from typing import TextIO
 
-x = [1, 2, 3]
-y = [1, 2, 3]
+def test():
+    x = [1, 2, 3]
+    y = [1, 2, 3]
 
-plt.plot(x, y, 'o', label="line1")
-plt.plot(x, y, label="Fit")
-plt.ylabel("YYYYYY")
-plt.show()
+    plt.plot(x, y, 'o', label="line1")
+    plt.plot(x, y, label="Fit")
+    plt.ylabel("YYYYYY")
+    plt.show()
+
+SAMPLE_TEST_PATH2 = 'sample/sample2.csv'
+SAMPLE_TEST_PATH3 = 'sample/sample3.csv'
 
 """
 Goal:
@@ -28,6 +35,54 @@ TODO:
 implement our gradient descent.
 
 """
+class Regression:
+    """
+    abstract class for a regression model 
+    """
+    def __init__(self, file: str) -> None:
+        self._file = file
+
+    def read_csv(self):
+        raise NotImplementedError
+
+    def gradient_descent(self, x: int, y: int) -> int:
+        """one iteration of gradient descent"""
+        raise NotImplementedError
+
+    def cost_function(self, x: int, y: int) -> int:
+        """MSE cost function"""
+        raise NotImplementedError
+
+    def display_graph(self):
+        raise NotImplementedError
+
+class LinearRegression(Regression):
+    """
+    Linear regression class (can we normalize to 0,1)?
+    """
+    
+    def __init__(self, file: str) -> None:
+        super().__init__(file)
+
+    def read_csv(self) -> tuple:
+        """Returning a tuple (x, y) of numpy arrays"""
+        df = pd.read_csv(self._file)
+        x = df['x'].to_numpy()
+        y = df['y'].to_numpy()
+        return (x, y)
+    
+    def display_graph(self):
+        """Display the data from csv as points on the plot"""
+        x, y = self.read_csv()
+        for i in range(len(x)):
+            plt.plot(x[i], y[i], 'o')
+        
+        plt.ylabel("Y")
+        plt.show()
+
+if __name__ == '__main__':
+    x = LinearRegression(SAMPLE_TEST_PATH3)
+    x.display_graph()
 
 
 
